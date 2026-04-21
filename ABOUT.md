433f37d2639ac73d537601424c274390721ee49d
MalikHw <help.malicorporation@gmail.com>
2026-04-21 12:07:16 +0100

    we made this!

---
+++ ABOUT.md
@@ -0,0 +1,8 @@
+Adds  blur behind pop ups and some UI elements! simple but cool
+
+altho it needs this mod to work
+
+<mod:thesillydoggo.blur-api>
+
+made by [MalikHw47](https://youtube.com/@MalikHw47)
+mod by someone i will mention next time cuz for now i dont have a logo lol
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,38 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/FLAlertLayer.hpp>
+#include <Geode/modify/DailyLevelPage.hpp>
+#include <Geode/modify/SetIDPopup.hpp>
+#include <Geode/modify/SetTextPopup.hpp>
+#include <Geode/modify/GJPathsLayer.hpp>
+
+#include "BlurAPI.hpp"
+
+using namespace geode::prelude;
+
+static void tryAddBlur(CCNode* node) {
+    if (Mod::get()->getSettingValue<bool>("enabled") && BlurAPI::isBlurAPIEnabled())
+        BlurAPI::addBlur(node);
+}
+
+class $modify(FLAlertLayer) {
+    void show() {
+        tryAddBlur(this); FLAlertLayer::show();
+    }
+    void destructor() {
+        BlurAPI::removeBlur(this); FLAlertLayer::~FLAlertLayer();
+    }
+    void onBtn1(CCObject* s) { BlurAPI::removeBlur(this); FLAlertLayer::onBtn1(s); }
+    void onBtn2(CCObject* s) { BlurAPI::removeBlur(this); FLAlertLayer::onBtn2(s); }
+};
+
+#define justBlur(ClassName)\
+class $modify(ClassName) {\
+    void show() {\
+        tryAddBlur(this); ClassName:show();\
+    }\
+};
+
+justBlur(DailyLevelPage)
+justBlur(SetIDPopup)
+justBlur(SetTextPopup)
+justBlur(GJPathsLayer)
