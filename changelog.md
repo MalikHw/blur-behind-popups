86933919c2c89901951bd5fd0d6ddece6f32a1d3
MalikHw <help.malicorporation@gmail.com>
2026-04-25 16:47:12 +0100

    ...

--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+### v1.1.1
+- fixed bugs that broke icon 107 and Custom Profiles
+---
+
 ### v1.1.0
 - used Alpha's Geode Utils now so code is easier and works better
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,40 @@
 #include <Geode/modify/FLAlertLayer.hpp>
 #include <alphalaneous.alphas_geode_utils/include/ObjectModify.hpp> // HOLY SHIT THAT'S EASIER
 #include "BlurAPI.hpp"
+#include <unordered_map>
 
 using namespace geode::prelude;
 
 static CCNode* backdropOf(FLAlertLayer* alert) {
     if (!alert) return nullptr;
     for (auto* n : alert->getChildrenExt<CCNode*>()) {
-        if (n && (typeinfo_cast<CCLayerColor*>(n) || typeinfo_cast<CCLayerGradient*>(n) || n->getZOrder() < 0))
+        if (n && (typeinfo_cast<CCLayerColor*>(n) || typeinfo_cast<CCLayerGradient*>(n)))
             return n;
     }
     return nullptr;
 }
 
-struct $baseModify(FLAlertLayer) {
-    void modify() {
-        auto self = reinterpret_cast<FLAlertLayer*>(this);
-        auto name = geode::cocos::getObjectName(self);
-        if (name == "ColorSelectLiveOverlay" || name == "HSVLiveOverlay" || name == "RewardUnlockLayer" || name == "RewardsPage" || name == "GJCommentListLayer")
-            return;
-        if (Mod::get()->getSettingValue<bool>("enabled") && BlurAPI::isBlurAPIEnabled())
-            if (auto bg = backdropOf(self))
-                BlurAPI::addBlur(bg);
-    }
-};
+static std::unordered_map<FLAlertLayer*, CCNode*> s_blurred;
 
 class $modify(FLAlertLayer) {
+    void show() {
+        FLAlertLayer::show();
+        auto name = geode::cocos::getObjectName(this);
+        if (name == "ColorSelectLiveOverlay" || name == "HSVLiveOverlay" || name == "RewardUnlockLayer" || name == "RewardsPage" || name == "GJCommentListLayer")
+            return;
+        if (!Mod::get()->getSettingValue<bool>("enabled") || !BlurAPI::isBlurAPIEnabled())
+            return;
+        if (auto bg = backdropOf(this)) {
+            BlurAPI::addBlur(bg);
+            s_blurred[this] = bg;
+        }
+    }
+
     void destructor() {
-        if (auto bg = backdropOf(this)) BlurAPI::removeBlur(bg);
+        if (auto it = s_blurred.find(this); it != s_blurred.end()) {
+            if (it->second) BlurAPI::removeBlur(it->second);
+            s_blurred.erase(it);
+        }
         FLAlertLayer::~FLAlertLayer();
     }
 };
